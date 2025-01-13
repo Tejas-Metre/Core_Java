c63d01dc768918d596897f05859ad030f4cfdaeb
Tejas-Metre <tejasametre1@gmail.com>
2025-01-13 20:30:24 +0530

    seventh commit

--- GitProgram/src/p1/TestClass.java
+++ GitProgram/src/p1/TestClass.java
@@ -3,6 +3,7 @@ package p1;
 public class TestClass {
 	public static void main(String[] args) {
 		System.out.println("*******This is a new class********");
+			System.out.println("*******This is a new core java program********");
 		
 	}
 
